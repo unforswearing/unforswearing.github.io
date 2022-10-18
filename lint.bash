e1e2b79be05ce6ff9c4026343f217d760dcf61b7
unforswearing <al.charity@gmail.com>
2022-10-18 14:09:02 -0400

    add contact

--- lint.bash
+++ lint.bash
@@ -3,6 +3,7 @@
 files=("index.html" "portfolio.html")
 
 for item in ${files}; do 
-  /usr/local/bin/tidy -indent -modify --drop-empty-elements no --wrap 80 --tidy-mark no --doctype strict "${item}"
+  :>|/dev/null 2>&1
+  # "$(command -v tidy)" -indent -modify --drop-empty-elements no --wrap 80 --tidy-mark no --doctype strict "${item}"
 done
 
