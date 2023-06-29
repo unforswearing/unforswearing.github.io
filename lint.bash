99c69295516575889508a2b59ad177f8b33b77e8
Alvin <al.charity@gmail.com>
2023-06-29 13:36:43 -0400

    add sample

--- lint.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/local/bin/bash
-
-files=("index.html" "portfolio.html")
-
-for item in ${files}; do 
-  :>|/dev/null 2>&1
-  # "$(command -v tidy)" -indent -modify --drop-empty-elements no --wrap 80 --tidy-mark no --doctype strict "${item}"
-done
-
