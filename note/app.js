7f93bd937b395b6e1486abfd7e405d53d9f99c1b
Alvin <al.charity@gmail.com>
2023-11-30 17:02:48 -0500

    reformat & fix accessibility pt. 1

--- note/app.js
+++ note/app.js
@@ -3,10 +3,8 @@
 // https://chalarangelo.github.io/parse-md-js/index.html
 // Replaces 'regex' with 'replacement' in 'str'
 // Curry function, usage: replaceRegex(regexVar, replacementVar) (strVar)
-const replaceRegex = function (regex, replacement) {
-  return function (str) {
-    return str.replace(regex, replacement);
-  }
+const replaceRegex = (regex, replacement) => function (str) {
+  return str.replace(regex, replacement);
 }
 // Regular expressions for Markdown (a bit strict, but they work)
 const codeBlockRegex = /((\n\t)(.*))+/g;
@@ -22,45 +20,35 @@ const unorderedListRegex = /(\n\s*(\-|\+)\s.*)+/g;
 const orderedListRegex = /(\n\s*([0-9]+\.)\s.*)+/g;
 const paragraphRegex = /\n+(?!<pre>)(?!<h)(?!<ul>)(?!<blockquote)(?!<hr)(?!\t)([^\n]+)\n/g;
 // Replacer functions for Markdown
-const codeBlockReplacer = function (fullMatch) {
-  return '\n<pre>' + fullMatch + '</pre>';
+const codeBlockReplacer = (fullMatch) => `\n<pre>${fullMatch}</pre>`
+const inlineCodeReplacer = (tagContents) => `<code>${tagContents}</code>`
+const imageReplacer = (tagTitle, tagURL) => `<img src="${tagURL}" alt="${tagTitle}" />`
+const linkReplacer = (tagTitle, tagURL) => `<a href="${tagURL}">${tagTitle}</a>`
+const strikethroughReplacer = (tagContents) => `<del>${tagContents}</del>`
+const blockquoteReplacer = (tagContents) => `\n<blockquote>${tagContents}</blockquote>`
+const paragraphReplacer = (tagContents) => `<p>${tagContents}</p>`
+const horizontalRuleReplacer = () => '\n<hr />'
+const headingReplacer = (tagStart, tagContents) => {
+  `\n<h${tagStart.trim().length}>${tagContents}</h${tagStart.trim().length}>`
 }
-const inlineCodeReplacer = function (fullMatch, tagStart, tagContents) {
-  return '<code>' + tagContents + '</code>';
+const boldItalicsReplacer = (tagStart, tagContents) => {
+  `<${(tagStart.trim().length == 1) ? ('em') : ('strong')}>
+    ${tagContents}
+   </${(tagStart.trim().length == 1) ? ('em') : ('strong')}>`
 }
-const imageReplacer = function (fullMatch, tagTitle, tagURL) {
-  return '<img src="' + tagURL + '" alt="' + tagTitle + '" />';
-}
-const linkReplacer = function (fullMatch, tagTitle, tagURL) {
-  return '<a href="' + tagURL + '">' + tagTitle + '</a>';
-}
-const headingReplacer = function (fullMatch, tagStart, tagContents) {
-  return '\n<h' + tagStart.trim().length + '>' + tagContents + '</h' + tagStart.trim().length + '>';
-}
-const boldItalicsReplacer = function (fullMatch, tagStart, tagContents) {
-  return '<' + ((tagStart.trim().length == 1) ? ('em') : ('strong')) + '>' + tagContents + '</' + ((tagStart.trim().length == 1) ? ('em') : ('strong')) + '>';
-}
-const strikethroughReplacer = function (fullMatch, tagStart, tagContents) {
-  return '<del>' + tagContents + '</del>';
-}
-const blockquoteReplacer = function (fullMatch, tagStart, tagContents) {
-  return '\n<blockquote>' + tagContents + '</blockquote>';
-}
-const horizontalRuleReplacer = function (fullMatch) {
-  return '\n<hr />';
-}
-const unorderedListReplacer = function (fullMatch) {
+const unorderedListReplacer = (fullMatch) => {
   let items = '';
-  fullMatch.trim().split('\n').forEach(item => { items += '<li>' + item.substring(2) + '</li>'; });
-  return '\n<ul>' + items + '</ul>';
+  fullMatch.trim().split('\n').forEach(item => { 
+    items += `<li>${item.substring(2)}</li>`; 
+  });
+  return `\n<ul>${items}</ul>`;
 }
-const orderedListReplacer = function (fullMatch) {
+const orderedListReplacer = (fullMatch) => {
   let items = '';
-  fullMatch.trim().split('\n').forEach(item => { items += '<li>' + item.substring(item.indexOf('.') + 2) + '</li>'; });
-  return '\n<ol>' + items + '</ol>';
-}
-const paragraphReplacer = function (fullMatch, tagContents) {
-  return '<p>' + tagContents + '</p>';
+  fullMatch.trim().split('\n').forEach(item => { 
+    items += `<li>${item.substring(item.indexOf('.') + 2)}</li>`; 
+  });
+  return `\n<ol>${items}</ol>`;
 }
 // Rules for Markdown parsing (use in order of appearance for best results)
 const replaceCodeBlocks = replaceRegex(codeBlockRegex, codeBlockReplacer);
@@ -77,9 +65,11 @@ const replaceOrderedLists = replaceRegex(orderedListRegex, orderedListReplacer);
 const replaceParagraphs = replaceRegex(paragraphRegex, paragraphReplacer);
 // Fix for tab-indexed code blocks
 const codeBlockFixRegex = /\n(<pre>)((\n|.)*)(<\/pre>)/g;
-const codeBlockFixer = function (fullMatch, tagStart, tagContents, lastMatch, tagEnd) {
+const codeBlockFixer = function (tagStart, tagContents, tagEnd) {
   let lines = '';
-  tagContents.split('\n').forEach(line => { lines += line.substring(1) + '\n'; });
+  tagContents.split('\n').forEach(line => {
+    lines += `${line.substring(1)}\n`; 
+  });
   return tagStart + lines + tagEnd;
 }
 const fixCodeBlocks = replaceRegex(codeBlockFixRegex, codeBlockFixer);
@@ -97,39 +87,21 @@ const replaceMarkdown = function (str) {
 // Parser for Markdown (fixes code, adds empty lines around for parsing)
 // Usage: parseMarkdown(strVar)
 const parseMarkdown = function (str) {
-  return fixCodeBlocks(replaceMarkdown('\n' + str + '\n')).trim();
+  return fixCodeBlocks(replaceMarkdown(`\n${str}\n`)).trim();
 }
 // -------------------------------------------------------------------
 // begin app.js
 const storage = localStorage;
 const action = {
-  get() {
-    return storage["action"];
-  },
-  load() {
-    storage.setItem("action", "load");
-  },
-  new() {
-    storage.setItem("action", "new");
-  },
-  save() {
-    storage.setItem("action", "save");
-  },
-  delete() {
-    storage.setItem("action", "delete");
-  },
-  removeAll() {
-    storage.setItem("action", "removeAll");
-  },
-  loadFile() {
-    storage.setItem("action", "loadFile");
-  },
-  loadNewFile() {
-    storage.setItem("action", "loadNewFile");
-  },
-  loadFileByUrl() {
-    storage.setItem("action", "loadFileByUrl");
-  },
+  get() { return storage["action"]; },
+  load() { storage.setItem("action", "load"); },
+  new() { storage.setItem("action", "new"); },
+  save() { storage.setItem("action", "save"); },
+  delete() { storage.setItem("action", "delete"); },
+  removeAll() { storage.setItem("action", "removeAll"); },
+  loadFile() { storage.setItem("action", "loadFile"); },
+  loadNewFile() { storage.setItem("action", "loadNewFile"); },
+  loadFileByUrl() { storage.setItem("action", "loadFileByUrl"); },
 };
 function messageData(title) {
   function errorMessage(message) {
