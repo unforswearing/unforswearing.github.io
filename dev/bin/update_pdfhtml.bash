0b15e89a8ec72cb7a5231bd8dbeccde35d0d4ae1
Alvin <al.charity@gmail.com>
2024-06-08 14:28:43 -0400

    final fix for pdf html

--- dev/bin/update_pdfhtml.bash
+++ dev/bin/update_pdfhtml.bash
@@ -3,7 +3,7 @@
 echo >| pdf.html && {
   echo "<ul>";
   ls portfolio/*pdf | while read pdf; do 
-    echo "  <li><a href=\"portfolio/$pdf\">$pdf</a></li>"; 
+    echo "  <li><a href=\"$pdf\">$pdf</a></li>"; 
   done;
   echo "</ul>";
   } >> pdf.html
