fc7b3ad4cddf0ded7b0c378c858116564bb73a50
Alvin <al.charity@gmail.com>
2024-06-08 14:31:53 -0400

    add tw to pdf html

--- dev/bin/update_pdfhtml.bash
+++ dev/bin/update_pdfhtml.bash
@@ -5,5 +5,8 @@ echo >| pdf.html && {
   ls portfolio/*pdf | while read pdf; do 
     echo "  <li><a href=\"$pdf\">$pdf</a></li>"; 
   done;
+  ls technical_writing_pdf/*pdf | while read pdf; do 
+    echo "  <li><a href=\"$pdf\">$pdf</a></li>"; 
+  done;
   echo "</ul>";
   } >> pdf.html
