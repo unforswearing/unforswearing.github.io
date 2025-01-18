4f6ed7dfbc124c4c182ea0eb49d9e3816b635d61
Alvin <al.charity@gmail.com>
2025-01-18 16:31:50 -0500

    Testing a smaller build script using zsh to prevent file overwrites

--- feed/build.bash
+++ feed/build.bash
@@ -1,55 +1,24 @@
-#!/bin/bash
+#!/bin/zsh
 
 set -x
 
-SITE_ROOT="/Users/unforswearing/Library/Mobile Documents/com~apple~CloudDocs/Documents/Scripts/Projects/unforswearing.github.io"
-BUILD_ROOT="${SITE_ROOT}/feed"
-LOG_ROOT="${BUILD_ROOT}/log"
-BK_ROOT="${BUILD_ROOT}/bk"
-
-cd "${BUILD_ROOT}" || exit
-
-# This build script keeps deleting the site index, not sure why.
-# First, back it up just in case.
-cp "${SITE_ROOT}/index.html" "${SITE_ROOT}/index.html.bk"
-
-# Then lock it to make sure nothing happens.
-chflags uchg "${SITE_ROOT}/index.html"
-
-cp "${BUILD_ROOT}/index.html" "${BK_ROOT}/index.html.bk"
-cp "${BUILD_ROOT}/feed.xml" "${BK_ROOT}/feed.xml.bk"
-cp "${BUILD_ROOT}/feed.xsl" "${BK_ROOT}/feed.xsl.bk"
+BUILD_ROOT="/Users/unforswearing/Library/Mobile Documents/com~apple~CloudDocs/Documents/Scripts/Projects/unforswearing.github.io/feed"
 
 /usr/local/bin/xml fo --dropdtd "${BUILD_ROOT}/feed.xml" > /tmp/feed.xml
-/usr/local/bin/xml fo --dropdtd "${BUILD_ROOT}/feed.xsl" > /tmp/feed.xsl
-
-rm "${BUILD_ROOT}/feed.xml" "${BUILD_ROOT}/feed.xsl"
-
 cat /tmp/feed.xml > "${BUILD_ROOT}/feed.xml"
+
+/usr/local/bin/xml fo --dropdtd "${BUILD_ROOT}/feed.xsl" > /tmp/feed.xsl
 cat /tmp/feed.xsl > "${BUILD_ROOT}/feed.xsl"
 
-rm "${BUILD_ROOT}/index.html"
 /usr/local/bin/xml tr "${BUILD_ROOT}/feed.xsl" "${BUILD_ROOT}/feed.xml" > "${BUILD_ROOT}/index.html"
 
-/usr/local/bin/tidy \
-  -indent \
-  -modify \
-  --wrap 80 \
-  --tidy-mark no \
-  --doctype strict \
-  -file "${LOG_ROOT}/tidy.log" \
-  "${BUILD_ROOT}/index.html"
+git add "${BUILD_ROOT}"
 
-# Unloc the index at site root.
-chflags nouchg "${SITE_ROOT}/index.html"
+printf "%s" "Enter Commit Message: "
+read -r commit_message
 
-# git add "${BUILD_ROOT}"
+git commit -m "${commit_message}"
 
-# printf "%s" "Enter Commit Message: "
-# read -r commit_message
+git push
 
-# git commit -m "${commit_message}"
-
-# git push
-
-# set +x
+set +x
