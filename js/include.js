3d421f03ac6186490074ef30ace2c24e4259b13a
Alvin <al.charity@gmail.com>
2025-05-06 17:16:30 -0400

    a working inlcude function / tag

---
+++ js/include.js
@@ -0,0 +1,10 @@
+(async function include() {
+  const objs = document.getElementsByTagName("include");
+  const tags = Array.from(objs);
+  for (tag of tags) {
+    const src = tag.getAttribute("src");
+    const data = await fetch(src);
+    const html = await data.text();
+    tag.outerHTML = html;
+  }
+})();
