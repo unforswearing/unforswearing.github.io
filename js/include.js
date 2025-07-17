18a380d35c75f89016797cc1ad9b8aa118d35775
Alvin <al.charity@gmail.com>
2025-07-17 18:07:10 -0400

    attempt

--- js/include.js
+++ js/include.js
@@ -1,7 +1,7 @@
 (async function include() {
   const objs = document.getElementsByTagName("include");
   const tags = Array.from(objs);
-  for (tag of tags) {
+  for (let tag of tags) {
     const src = tag.getAttribute("src");
     const data = await fetch(src);
     const html = await data.text();
