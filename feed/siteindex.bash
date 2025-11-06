af4f6fa0fea4e7f7c4115a1184e998f06063f97a
Alvin <al.charity@gmail.com>
2025-11-05 23:19:12 -0500

    experiment done

--- feed/siteindex.bash
+++
@@ -1,5 +0,0 @@
-
-source "roots.bash"
-
-SITE_INDEX="https://raw.githubusercontent.com/unforswearing/unforswearing.github.io/4d9bbc9b22c7d3a7380aac0327e69206d165d661/index.html"
-curl -s "${SITE_INDEX}" -o "${SITE_ROOT}/index.html"
